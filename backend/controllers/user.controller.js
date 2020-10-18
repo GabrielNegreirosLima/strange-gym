414a633f349f65d0b8a94e86f70ef9d3e5891ad2
Gabriel Negreiros Lima <gabrielnegreiroslima@gmail.com>
2020-10-18 15:46:36 -0300

    Add controllers schema

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,65 @@
+const db = require("../models");
+const Tutorial = db.tutorials;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new User
+exports.create = (req, res) => {
+  
+  if (!req.body.name) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+    return;
+  }
+
+  const user = {
+    name: req.body.name
+  };
+
+  User.create(user)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Tutorial."
+      });
+    });
+};
+
+// Retrieve all Users from the database.
+exports.findAll = (req, res) => {
+  
+};
+
+// Find a single User with an id
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  User.findByPk(id)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving Tutorial with id=" + id
+      });
+    });
+};
+
+// Update a User by the id in the request
+exports.update = (req, res) => {
+  
+};
+
+// Delete a User with the specified id in the request
+exports.delete = (req, res) => {
+  
+};
+
+// Delete all Users from the database.
+exports.deleteAll = (req, res) => {
+  
+};
+
