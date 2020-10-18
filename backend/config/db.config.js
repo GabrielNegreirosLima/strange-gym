8f7f243cf568de57de78ccfb41fbc4f68c82824b
Gabriel Negreiros Lima <gabrielnegreiroslima@gmail.com>
2020-10-18 14:30:33 -0300

    Add first server.js commit and sequelize config

---
+++ backend/config/db.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  HOST: "localhost",
+  USER: "postgres",
+  PASSWORD: "awesomepostgres",
+  DB: "postgres",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.tutorials = require("./tutorial.model.js")(sequelize, Sequelize);
+
+module.exports = db;
+
